929ed244bfd82cf69f094478c132a010f5987944
Venu Renangi <88401367+venuvvv111@users.noreply.github.com>
2023-03-20 20:33:28 +0530

    Add files via upload

---
+++ class.dart
@@ -0,0 +1,33 @@
+class Article {
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+
+  //Now let's create the constructor
+  Article({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+
+  //And now let's create the function that will map the json into a list
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      author: json['author'] as String,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      url: json['url'] as String,
+      urlToImage: json['urlToImage'] as String,
+      publishedAt: json['publishedAt'] as String,
+      content: json['content'] as String,
+    );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,100 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'class.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'News App',
+      home: NewsList(),
+    );
+  }
+}
+
+class NewsList extends StatefulWidget {
+  const NewsList({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _NewsListState createState() => _NewsListState();
+}
+
+class _NewsListState extends State<NewsList> {
+  late Future<List<Article>> futureArticles;
+
+  @override
+  void initState() {
+    super.initState();
+    futureArticles = getNewsData();
+  }
+
+  Future<List<Article>> getNewsData() async {
+    const String url =
+        "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4f63681140c84fe6a9d781bedb2e4472";
+
+    try {
+      http.Response response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final List<Article> articles = [];
+        Map<String, dynamic> jsonData = json.decode(response.body);
+
+        for (var item in jsonData["articles"]) {
+          articles.add(Article.fromJson(item));
+        }
+
+        return articles;
+      } else {
+        throw Exception("Failed to load news data");
+      }
+    } catch (e) {
+      throw Exception("Failed to load news data");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Latest News'),
+      ),
+      body: FutureBuilder<List<Article>>(
+        future: futureArticles,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Article>? articles = snapshot.data;
+            return ListView.builder(
+              itemCount: articles!.length,
+              itemBuilder: (context, index) {
+                Article article = articles[index];
+                return ListTile(
+                  title: Text(article.title.toString()),
+                  subtitle: Text(article.description.toString()),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text('Failed to load news data'),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
